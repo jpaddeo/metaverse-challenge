9882bda56badebce9ec8b45b86beea7f8db15032
jpaddeo <jpaddeo@gmail.com>
2021-12-27 23:27:15 -0300

    Advancing day2. Metamask Login

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import { useMoralis } from 'react-moralis';
+
+import Login from '../components/Login';
+
+export default function Home() {
+  const { isAuthenticated, logout } = useMoralis();
+
+  if (!isAuthenticated) return <Login />;
+
+  return (
+    <div className='h-screen'>
+      <Head>
+        <title>Metaverse Challenge</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1>Welcome to metaverse challenge!</h1>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
